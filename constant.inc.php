b549fede1269172888ad1f6df4abdf918441dc3b
Ravi Kumar <ravi14577@gmail.com>
2021-06-10 06:42:20 +0530

    food

--- constant.inc.php
+++ constant.inc.php
@@ -1,9 +1,9 @@
 <?php
 define('SITE_NAME','Food Ordering Admin');
-define('FRONT_SITE_NAME','Food Ordering');
+define('FRONT_SITE_NAME','Food_Ordering');
 
-define('FRONT_SITE_PATH','http://127.0.0.1/php/food_ordering/');
-define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']."/php/food_ordering/");
+define('FRONT_SITE_PATH','http://localhost:8080/food_ordering/');
+define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']."/food_ordering/");
 
 define('SERVER_DISH_IMAGE',SERVER_IMAGE."media/dish/");
 define('SITE_DISH_IMAGE',FRONT_SITE_PATH."media/dish/");
